a08509910daa6b3fe78ca2992f49bd7c13ed8a82
IrbisX7 <157482091+Irbisovich@users.noreply.github.com>
2025-02-04 17:46:56 +0300

    Update main.py
    Added 'Testing BIOS' (added command 'info')

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #opBrowser Beta v0.1
 
 import pyfiglet
+import requests
+import os
 
 class OPBrowser:
     class Core:
@@ -12,11 +14,36 @@ class OPBrowser:
             def __str__(self):
                 return f"{self.message} (Error Code: {self.error_code})"
 
-        def __init__(self, bios_enabled):
+        def __init__(self, bios_enabled, version, version_name, browser_creator, browser_name):
             if bios_enabled == True:
-                self.font = 'kban'
+                print('\n' + self.fixed_figlet_format('opBrowser', f'{version_name} v{version} Testing Bios', 'kban'))
 
-                print('\n' + self.fixed_figlet_format('opBrowser', 'Beta v0.1 Testing Bios', self.font))
+                while True:
+                    self.user = os.environ['USERNAME']
+                    self.line = str(input(f'{self.user}: '))
+
+                    try:
+                        self.command, self.attribute = self.line.strip(' ')
+                    except ValueError:
+                        self.command = self.line
+
+                    if self.command == 'info':
+                        self.info = {
+                            'browser_name': f'Browser name: {browser_name}',
+                            'version': f'Version: {version}',
+                            'browser_creator': f'Browser creator: {browser_creator}'
+                        }
+
+                        self.info_list = []
+                        for info in self.info:
+                            self.info_list.append(self.info[info])
+
+                        self.info_list = '\n'.join(self.info_list)
+
+                        print(f'\n{self.info_list}\n')
+
+                    else:
+                        raise self.ErrorRaiser('Wrong command!', 2)
 
             elif bios_enabled == False:
                 pass
@@ -50,7 +77,7 @@ class OPBrowser:
 
             return '\n'.join(strings)
 
-    def __init__(self, bios_enabled=False):
-        self.core = self.Core(bios_enabled)
+    def __init__(self, bios_enabled=False, version='Dev', version_name='Beta', browser_creator='IrbisX7', browser_name='opBrowser'):
+        self.core = self.Core(bios_enabled, version, version_name, browser_creator, browser_name)
 
 OPBrowser(True)
